4a6a1ec774d10fe088a9e64671638325fb39af9d
Nindo Dev <nindo.developer@gmail.com>
2015-09-28 23:23:53 -0400

    Use same view for tabs and inflate it

--- app/src/main/java/com/nindo/manga/reader/HomeActivity.java
+++ app/src/main/java/com/nindo/manga/reader/HomeActivity.java
@@ -4,21 +4,17 @@ package com.nindo.manga.reader;
  * Created by NindoDev on 9/26/2015.
  */
 
-import android.content.res.Configuration;
-import android.support.design.widget.CoordinatorLayout;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -61,8 +57,7 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
 
